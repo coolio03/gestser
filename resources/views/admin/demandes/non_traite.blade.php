6041c4d082d80b830cc7abab037776e3236789ba
coolio03 <COULIBALY@COOL>
2021-01-29 00:59:16 +0000

    change trustproxies

--- resources/views/admin/demandes/non_traite.blade.php
+++ resources/views/admin/demandes/non_traite.blade.php
@@ -153,7 +153,7 @@
                                                 @endif
                                                     
                                                 
-                                                @if ($dde->date_remise_ra==null)
+                                                @if (empty($dde->date_remise_ra))
                                                     @include('partials.form-group',[
                                                         'title'=>__('Date de remise RA'),
                                                         'type'=>'date',
@@ -169,7 +169,7 @@
                                                     ])
                                                 @endif     
                                             
-                                                @if ($dde->date_traitement==null)
+                                                @if (empty($dde->date_traitement))
                                                     @include('partials.form-group',[
                                                         'title'=>__('Date de traitement'),
                                                         'type'=>'date',
