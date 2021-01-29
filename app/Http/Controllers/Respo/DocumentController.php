1aff6bd59e68a31b3f9bae4b8f24cdedbe1a9054
coolio03 <COULIBALY@COOL>
2021-01-29 16:08:27 +0000

    redige note embauche

--- app/Http/Controllers/Respo/DocumentController.php
+++ app/Http/Controllers/Respo/DocumentController.php
@@ -115,7 +115,7 @@ class DocumentController extends Controller
     {
         setlocale(LC_TIME, 'fra_fra');
         $desc = Demande::find($demande->id);
-        $my_template = new \PhpOffice\PhpWord\TemplateProcessor(public_path("Documents/EMBAUCHE A L ESSAI/DCRH IS 71 25 01 NOTE D'NFO EMBAUCHE EO M.doc"));
+        $my_template = new \PhpOffice\PhpWord\TemplateProcessor(public_path("Documents/EMBAUCHE_A_L_ESSAI/NOTE_EMBAUCHE.doc"));
         $my_template->setValue('date_redaction',strftime('%d %B %Y'));
         $my_template->setValue('emetteur',strtoupper($desc->user->name) );
         $my_template->setValue('destinataire',$request->destinataire);
