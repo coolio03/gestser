a9f843606d2c08ad2edd0ad93aeba4ae1387a4a4
coolio03 <COULIBALY@COOL>
2021-01-31 10:11:31 +0000

    Document controler document contrat CDI

--- app/Http/Controllers/Respo/DocumentController.php
+++ app/Http/Controllers/Respo/DocumentController.php
@@ -264,7 +264,7 @@ class DocumentController extends Controller
     {
         setlocale(LC_TIME, 'fra_fra');
         $desc = Demande::find($demande->id);
-        $my_template = new \PhpOffice\PhpWord\TemplateProcessor(public_path("Documents/EMBAUCHE_A_L_ESSAI/DCRH IS 71 01 02 Contrat CDI.docx"));
+        $my_template = new \PhpOffice\PhpWord\TemplateProcessor(public_path("Documents/CDI/Contrat_CDI.docx"));
         $my_template->setValue('date_redaction',strftime('%d %B %Y'));
         $my_template->setValue('emetteur',strtoupper($desc->user->name) );
         $my_template->setValue('civilite', ucfirst($desc->collaborateur->civilite));
