4d93fcf4a3c6d6cf6d1b27c50293f435462a4e23
coolio03 <COULIBALY@COOL>
2021-02-01 03:20:40 +0000

    ajout de delai

--- resources/views/respo/demandes/liste.blade.php
+++ resources/views/respo/demandes/liste.blade.php
@@ -54,7 +54,7 @@
                                 @endif 
                             </td>
                             <td>
-                                {{$delai = (new DateTime($dde->date_remise_ra))->diff(new DateTime($dde->date_traitement))}}
+                                {{$delai = Carbon\Carbon::parse($dde->date_remise_ra)->diffInDays(Carbon\Carbon::parse($ddate_traitement))}}
                             </td>
                             <td  @if ($dde->date_traitement == null))
                                 style="background-color: silver"
