093d1821a2cf27356bdec01772eefa408af59603
coolio03 <COULIBALY@COOL>
2021-02-01 09:09:26 +0000

    modification des controller demande et collabo

--- config/database.php
+++ config/database.php
@@ -61,7 +61,7 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8mb4',
+            'charset' => 'utf8',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'schema' => 'public',
