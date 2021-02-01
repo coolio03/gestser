65e4be3d58e491a32ec686f84ff69bb77be6843f
coolio03 <COULIBALY@COOL>
2021-02-01 09:28:53 +0000

    modification des controller demande et collabo

--- app/Http/Controllers/Respo/DocumentController.php
+++ app/Http/Controllers/Respo/DocumentController.php
@@ -101,7 +101,7 @@ class DocumentController extends Controller
         setlocale(LC_TIME, 'French');
         $desc = Demande::find($demande->id);   
         $my_template = new \PhpOffice\PhpWord\TemplateProcessor(public_path("Documents/STAGE/ATTESTATION_STAGE.docx"));
-        $my_template->setValue('date_redaction',strftime('%d %B %Y'));
+        $my_template->setValue('date_redaction',Carbon::formatLocalized('%d %B %Y'));
         $my_template->setValue('emetteur',strtoupper($desc->user->name) );
         $my_template->setValue('civilite', ucfirst($desc->collaborateur->civilite));
         $my_template->setValue('initial', implode('',array_map(function($p){return strtoupper($p[0]);},explode(' ',$desc->user->name))));
