f471270b045344191251510502f8c441be587ff3
coolio03 <COULIBALY@COOL>
2021-02-02 00:46:39 +0000

    add CompteController

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -135,7 +135,7 @@
             </a>
           </li>
           <li class="nav-item ">
-            <a href="#" class="nav-link ">
+            <a href=" {{route('admin.comptes.index')}} " class="nav-link ">
               <i class="nav-icon fas fa-user"></i>
               <p>
                 Comptes
