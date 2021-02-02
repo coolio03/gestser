9f56feaabb1f603c3b7a101573590496edbcc74f
coolio03 <COULIBALY@COOL>
2021-02-02 22:13:27 +0000

    changer user user login

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -26,7 +26,7 @@ class RedirectIfAuthenticated
 
 
 
-        if (Auth::guard($guard)->check() && auth()->user()->role == 1) {
+        if (Auth::guard($guard)->check() && Auth::guard()->role == 1) {
             return redirect()->route('home');
         }
 
