438bfc19a812f665da55c0fa3a231c0ac3250e39
coolio03 <COULIBALY@COOL>
2021-02-09 08:42:33 +0000

    add compteController

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -204,7 +204,7 @@
             </ul>
           </li>
           <li class="nav-item has-treeview">
-            <a href="#" class="nav-link @if(!$segment)
+            <a href="#" class="nav-link @if($segment=='Suivis des demandes')
             active
           @endif">
               <i class="nav-icon fas fa-chart-pie"></i>
