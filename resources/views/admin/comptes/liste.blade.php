da0377492604fcb05340e56f3098b8988a31a3b1
coolio03 <COULIBALY@COOL>
2021-02-10 15:03:59 +0000

    add compte

--- resources/views/admin/comptes/liste.blade.php
+++ resources/views/admin/comptes/liste.blade.php
@@ -33,6 +33,7 @@
             <p>
                 <a href=" {{route('admin.comptes.create')}} " class="btn btn-primary"><i class="nav-icon fas fa-plus"></i>&nbsp;&nbsp;Ajouter un compte</a>
             </p>
+            <a href=" {{route('admin.comptes.create')}} ">Acceuil</a>
         <div class="card-body"> 
             <table id="example1" class="table table-bordered table-striped">
                 <thead>
